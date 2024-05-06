7a83180d0109572ccbcc17891ec6670c81fba921
VictorJoss <vramirezgarcia64@correo.unicordoba.edu.co>
2024-05-06 00:57:48 -0500

    first commit

---
+++ src/main/java/dev/victor/api/consumption/ElectricalConsumptionApiApplication.java
@@ -0,0 +1,13 @@
+package dev.victor.api.consumption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricalConsumptionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricalConsumptionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/victor/api/consumption/ElectricalConsumptionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.victor.api.consumption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricalConsumptionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
