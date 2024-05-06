96702219ad26d6fca588eb48bd3575eb8198b67e
VictorJoss <vramirezgarcia64@correo.unicordoba.edu.co>
2024-05-06 02:06:31 -0500

    Add getConsumptionByDate method

--- src/main/java/dev/victor/api/consumption/services/IConsumptionService.java
+++ src/main/java/dev/victor/api/consumption/services/IConsumptionService.java
@@ -3,8 +3,10 @@ package dev.victor.api.consumption.services;
 import dev.victor.api.consumption.entities.ElectricityConsumption;
 
 import java.util.List;
+import java.util.Map;
 
 public interface IConsumptionService {
 
     List<ElectricityConsumption> findAll();
+    Map<String, Double> getConsumptionByDate(String meterDate);
 }
--- src/main/java/dev/victor/api/consumption/services/impl/ConsumptionServiceImpl.java
+++ src/main/java/dev/victor/api/consumption/services/impl/ConsumptionServiceImpl.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.stream.IntStream;
 
 @Service
 public class ConsumptionServiceImpl implements IConsumptionService {
@@ -17,5 +21,34 @@ public class ConsumptionServiceImpl implements IConsumptionService {
     @Override
     public List<ElectricityConsumption> findAll() {
         return consumptionRepository.findAll();
+    }
+
+    @Override
+    public Map<String, Double> getConsumptionByDate(String meterDate) {
+
+        SortedMap<String, Double> consumptionPerHour = new TreeMap<>();
+        List<ElectricityConsumption> consumptionPerDay = consumptionRepository.findByMeterDate(meterDate);
+
+        IntStream.range(0, 24)
+                .mapToObj(hour -> String.format("%02d", hour))
+                .forEach(hour -> {
+                    double maximumConsumption = consumptionPerDay.stream()
+                            .filter(consumption -> consumption.getMeterHour().startsWith(hour))
+                            .mapToDouble(ElectricityConsumption::getActiveEnergy)
+                            .max()
+                            .orElse(0.0);
+
+                    double minimumConsumption = consumptionPerDay.stream()
+                            .filter(consumption -> consumption.getMeterHour().startsWith(hour))
+                            .mapToDouble(ElectricityConsumption::getActiveEnergy)
+                            .min()
+                            .orElse(0.0);
+
+                    double dailyConsumption = maximumConsumption - minimumConsumption;
+
+                    consumptionPerHour.put(hour + ":00:00", dailyConsumption);
+                });
+
+        return consumptionPerHour;
     }
 }
