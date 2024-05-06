7d5a870177326afb901f8467d702bbba3f08cb16
VictorJoss <vramirezgarcia64@correo.unicordoba.edu.co>
2024-05-06 02:03:28 -0500

    Add findByMeterDate method to ElectricityConsumption repository

--- src/main/java/dev/victor/api/consumption/repository/IConsumptionRepository.java
+++ src/main/java/dev/victor/api/consumption/repository/IConsumptionRepository.java
@@ -2,8 +2,14 @@ package dev.victor.api.consumption.repository;
 
 import dev.victor.api.consumption.entities.ElectricityConsumption;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IConsumptionRepository extends JpaRepository<ElectricityConsumption, Long>{
+
+    @Query(value = "SELECT * FROM CONSUMPTIONS WHERE METER_DATE=:meterDate", nativeQuery = true)
+    List<ElectricityConsumption> findByMeterDate(String meterDate);
 }
