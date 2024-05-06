524d4dd52612504d49fd74b778148bc266afdfd1
VictorJoss <vramirezgarcia64@correo.unicordoba.edu.co>
2024-05-06 02:09:49 -0500

    Add endpoint for retrieving daily consumption data

--- src/main/java/dev/victor/api/consumption/controllers/ConsumptionController.java
+++ src/main/java/dev/victor/api/consumption/controllers/ConsumptionController.java
@@ -1,14 +1,17 @@
 package dev.victor.api.consumption.controllers;
 
 import dev.victor.api.consumption.entities.ElectricityConsumption;
+import dev.victor.api.consumption.entities.RequestData;
 import dev.victor.api.consumption.services.IConsumptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/consumption")
@@ -19,8 +22,22 @@ public class ConsumptionController {
 
     @GetMapping("/all")
     public ResponseEntity<List<ElectricityConsumption>> findAll() {
+
         try {
             return ResponseEntity.ok(consumptionService.findAll());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping("/daily")
+    public ResponseEntity<Map<String, Double>> getDailyConsumption(@RequestBody RequestData requestData) {
+
+        try {
+            if (requestData.getPeriod().equalsIgnoreCase("daily")) {
+                return ResponseEntity.ok(consumptionService.getConsumptionByDate(requestData.getDate()));
+            }
+            return ResponseEntity.badRequest().build();
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
